7e546dfb363c956cf98dccb0f17ee8b5826d0e08
Gowtham <gow8004@gmail.com>
2024-02-12 11:54:45 +0530

    functional comp

--- portfolio/src/components/Experience.js
+++ portfolio/src/components/Experience.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import {
   VerticalTimeline,
   VerticalTimelineElement,
@@ -6,61 +6,60 @@ import {
 import "react-vertical-timeline-component/style.min.css";
 import Badge from "react-bootstrap/Badge";
 
-class Experience extends Component {
-  render() {
-    if (this.props.resumeExperience && this.props.resumeBasicInfo) {
-      var sectionName = this.props.resumeBasicInfo.section_name.experience;
-      var work = this.props.resumeExperience.map(function (work, i) {
-        const technologies = work.technologies;
-        const mainTechnologies = work.mainTech;
+const Experience = (props) => {
+  const { resumeExperience, resumeBasicInfo } = props;
 
-        var mainTech = mainTechnologies.map((technology, i) => {
-          return (
-            <Badge pill className="main-badge mr-2 mb-2" key={i}>
-              {technology}
-            </Badge>
-          );
-        });
-        var tech = technologies.map((technology, i) => {
-          return (
-            <Badge pill className="experience-badge mr-2 mb-2" key={i}>
-              {technology}
-            </Badge>
-          );
-        });
-        return (
-          <VerticalTimelineElement
-            className="vertical-timeline-element--work"
-            date={work.years}
-            iconStyle={{
-              background: "#AE944F",
-              color: "#fff",
-              textAlign: "center",
-            }}
-            icon={<i className="fab fa-angular experience-icon"></i>}
-            key={i}
+  if (resumeExperience && resumeBasicInfo) {
+    const sectionName = resumeBasicInfo.section_name.experience;
+
+    const workElements = resumeExperience.map((work, i) => {
+      const technologies = work.technologies;
+      const mainTechnologies = work.mainTech;
+
+      const mainTech = mainTechnologies.map((technology, i) => (
+        <Badge pill className="main-badge mr-2 mb-2" key={i}>
+          {technology}
+        </Badge>
+      ));
+
+      const tech = technologies.map((technology, i) => (
+        <Badge pill className="experience-badge mr-2 mb-2" key={i}>
+          {technology}
+        </Badge>
+      ));
+
+      return (
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          date={work.years}
+          iconStyle={{
+            background: "#AE944F",
+            color: "#fff",
+            textAlign: "center",
+          }}
+          icon={<i className="fab fa-angular experience-icon"></i>}
+          key={i}
+        >
+          <div style={{ textAlign: "left", marginBottom: "4px" }}>
+            {mainTech}
+          </div>
+
+          <h3
+            className="vertical-timeline-element-title"
+            style={{ textAlign: "left" }}
           >
-            <div style={{ textAlign: "left", marginBottom: "4px" }}>
-              {mainTech}
-            </div>
-
-            <h3
-              className="vertical-timeline-element-title"
-              style={{ textAlign: "left" }}
-            >
-              {work.title}
-            </h3>
-            <h4
-              className="vertical-timeline-element-subtitle"
-              style={{ textAlign: "left" }}
-            >
-              {work.company}
-            </h4>
-            <div style={{ textAlign: "left", marginTop: "15px" }}>{tech}</div>
-          </VerticalTimelineElement>
-        );
-      });
-    }
+            {work.title}
+          </h3>
+          <h4
+            className="vertical-timeline-element-subtitle"
+            style={{ textAlign: "left" }}
+          >
+            {work.company}
+          </h4>
+          <div style={{ textAlign: "left", marginTop: "15px" }}>{tech}</div>
+        </VerticalTimelineElement>
+      );
+    });
 
     return (
       <section id="resume" className="pb-5">
@@ -75,7 +74,7 @@ class Experience extends Component {
         </div>
         <div className="col-md-8 mx-auto">
           <VerticalTimeline>
-            {work}
+            {workElements}
             <VerticalTimelineElement
               iconStyle={{
                 background: "#AE944F",
@@ -91,6 +90,8 @@ class Experience extends Component {
       </section>
     );
   }
-}
+
+  return null;
+};
 
 export default Experience;
--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -1,44 +1,40 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Skills extends Component {
-  render() {
-    if (this.props.sharedSkills && this.props.resumeBasicInfo) {
-      var sectionName = this.props.resumeBasicInfo.section_name.skills;
-      var skills = this.props.sharedSkills.icons.map(function (skills, i) {
-        return (
-          <li className="list-inline-item mx-3" key={i}>
-            <span>
-              <div className="text-center skills-tile">
-                <i className={skills.class} style={{ fontSize: "220%" }}>
-                  <p
-                    className="text-center"
-                    style={{ fontSize: "30%", marginTop: "4px",fontFamily:"Inter, sans-serif" }}
-                  >
-                    {skills.name}
-                  </p>
-                </i>
-              </div>
-            </span>
-          </li>
-        );
-      });
-    }
-
-    return (
-      <section id="skills">
-        <div className="col-md-12">
-          <div className="col-md-12">
-            <h1 className="section-title">
-              <span className="text-white">{sectionName}</span>
-            </h1>
+const Skills = (props) => {
+  if (props.sharedSkills && props.resumeBasicInfo) {
+    var sectionName = props.resumeBasicInfo.section_name.skills;
+    var skills = props.sharedSkills.icons.map((skill, i) => (
+      <li className="list-inline-item mx-3" key={i}>
+        <span>
+          <div className="text-center skills-tile">
+            <i className={skill.class} style={{ fontSize: "220%" }}>
+              <p
+                className="text-center"
+                style={{ fontSize: "30%", marginTop: "4px", fontFamily: "Inter, sans-serif" }}
+              >
+                {skill.name}
+              </p>
+            </i>
           </div>
-          <div className="col-md-12 text-center">
-            <ul className="list-inline mx-auto skill-icon">{skills}</ul>
-          </div>
-        </div>
-      </section>
-    );
+        </span>
+      </li>
+    ));
   }
-}
+
+  return (
+    <section id="skills">
+      <div className="col-md-12">
+        <div className="col-md-12">
+          <h1 className="section-title">
+            <span className="text-white">{sectionName}</span>
+          </h1>
+        </div>
+        <div className="col-md-12 text-center">
+          <ul className="list-inline mx-auto skill-icon">{skills}</ul>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Skills;
